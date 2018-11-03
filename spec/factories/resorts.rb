156d7859dd51dff56cda0b80ce11ccb6bdee3859
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-03 17:02:33 -0600

    Add factory for resorts [#1]

---
+++ spec/factories/resorts.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :resort do
+    title { "Crested Butte" }
+    address { "MyString" }
+    latitude { "MyString" }
+    longitude { "MyString" }
+    visited_by { "MyString"}
+  end
+end
