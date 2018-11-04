049fbd6704f8fbf0cea7a21fe13b38961b755c30
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-03 21:37:45 -0600

    Dream drive snowcast controller refactor [#1]

--- app/controllers/api/v1/snowcast_controller.rb
+++ app/controllers/api/v1/snowcast_controller.rb
@@ -1,17 +1,17 @@
 class Api::V1::SnowcastController < ApplicationController
   def index
-    # binding.pry
     resort = Resort.find_by(title: params[:location])
-    # binding.pry
-    conn = Faraday.new(url: "https://api.darksky.net") do |faraday|
-      faraday.adapter Faraday.default_adapter
-    end
-    # latitude = 42.3601
-    # longitude = -71.0589
-    response = conn.get("/forecast/#{ENV["DARKSKY_API_KEY"]}/#{resort.latitude},#{resort.longitude}")
-    data = JSON.parse(response.body)
-    snowcast = Snowcast.new(data)
+    snowcast = SnowcastFacade.new(resort)
+
+      conn = Faraday.new(url: "https://api.darksky.net") do |faraday|
+        faraday.adapter Faraday.default_adapter
+      end
+
+      response = conn.get("/forecast/#{ENV["DARKSKY_API_KEY"]}/#{resort.latitude},#{resort.longitude}")
+      data = JSON.parse(response.body)
+      snowcast = Snowcast.new(data)
 
     render json: snowcast
   end
+
 end
