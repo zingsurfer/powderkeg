04520c142dd5256b3e72b4d0050368c95f6dceb9
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-03 19:29:12 -0600

    Renamed snowcast request spec [#1]

--- spec/requests/api/v1/snowcast_request_spec.rb
+++ spec/requests/api/v1/snowcast_request_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 describe 'Snowcast API' do
   it 'returns a snowcast' do
     resort = create(:resort)
-    snowcast = create(:snowcast, resort_id: resort.id)
+    create(:snowcast, id: 1, resort_id: resort.id)
 
     get "/api/v1/snowcast?location=#{resort.title}"
 
+    snowcast = JSON.parse(response.body)
     expect(response).to be_successful
+    expect(snowcast.id).to eq(1)
   end
 end
