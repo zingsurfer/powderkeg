fa06ae62f99fb502097338fbc7d3857a1a595ca9
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-03 20:34:35 -0600

    Make successful API call & render the data [#1]

--- app/controllers/api/v1/snowcast_controller.rb
+++ app/controllers/api/v1/snowcast_controller.rb
@@ -6,9 +6,9 @@ class Api::V1::SnowcastController < ApplicationController
     latitude = 42.3601
     longitude = -71.0589
     response = conn.get("/forecast/#{ENV["DARKSKY_API_KEY"]}/#{latitude},#{longitude}")
-    binding.pry
-    
+    data = JSON.parse(response.body)
+    snowcast = Snowcast.new(data)
 
-    render json: Snowcast.all
+    render json: snowcast
   end
 end
