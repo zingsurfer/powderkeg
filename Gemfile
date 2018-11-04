2d62f5cfe3fdb67bb7d95f26ae9096a5967c29ce
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-04 12:24:31 -0700

    Complete simplecov set up [#1]

--- Gemfile
+++ Gemfile
@@ -43,6 +43,10 @@ group :development, :test do
   gem 'factory_bot_rails'
 end
 
+group :test do
+  gem 'simplecov', require: false
+end
+
 group :development do
   gem 'listen', '>= 3.0.5', '< 3.2'
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,9 +5,10 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'simplecov'
 require 'support/factory_bot'
 # Add additional requires below this line. Rails is not loaded until this point!
-SimplCov.start "rails"
+SimpleCov.start "rails"
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
