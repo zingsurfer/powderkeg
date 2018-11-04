6e32c8d1a9d51c8b0d1505041722270b5600156c
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-03 21:23:58 -0600

    Update resort factory with latitude & longitude [#1]

--- spec/factories/resorts.rb
+++ spec/factories/resorts.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :resort do
     title { "Crested Butte" }
-    address { "MyString" }
-    latitude { "MyString" }
-    longitude { "MyString" }
+    address { "12 Snowmass Rd, Crested Butte, CO 81225" }
+    latitude { 38.8833298 }
+    longitude { -106.939496242 }
     visited_by { "MyString"}
   end
 end
