aaeab4b7a7712116d4934e84dfb63a47dc65126a
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-03 20:33:27 -0600

    Update snowcast to be a poro [#1]

--- app/models/snowcast.rb
+++ app/models/snowcast.rb
@@ -1,3 +1,21 @@
-class Snowcast < ApplicationRecord
-  belongs_to :resort
+class Snowcast
+  attr_reader :feel_temp
+  def initialize(data = {})
+    @feel_temp = data["currently"]["apparentTemperature"]
+     # @apparent_temp
+     # @apparent_temp_max
+     # @apparent_temp_max_time
+     # @cloud_cover
+     # @precip_accumulation
+     # @precip_intensity_max
+     # @precip_intensity_max_time
+     # @precip_probability
+     # @precip_type
+     # @visibility
+     # @temp_max
+     # @temp_max_time
+     # @wind_gust
+     # @wind_gust_time
+     # @wind_speed
+  end
 end
