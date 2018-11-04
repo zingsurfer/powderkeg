9a22b54148c106a8d95789f0fd433b0da4971ac9
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-03 21:22:33 -0600

    Add more resorts to see data for [#1]

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,34 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Resort.create!(
-  title: "Breckenridge",
+  title: "breckenridge",
   address: "1599 Ski Hill Rd, Breckenridge, CO 80424"
 )
 Resort.create!(
-  title: "Keystone",
+  title: "keystone",
   address: "1202 Jones Gulch Rd, Keystone, CO 80435"
+)
+Resort.create!(
+  title: "arapahoe_basin",
+  address: "28194 US-6, Keystone, CO 80435",
+)
+Resort.create!(
+  title: "crested_butte",
+  latitude: 38.8833298,
+  longitude: -106.939496242
+)
+Resort.create!(
+  title: "vail",
+  latitude: 39.6403,
+  longitude: -106.3742,
+)
+Resort.create!(
+  title: "winter_park",
+  latitude: 39.8717,
+  longitude: -105.7631,
+)
+Resort.create!(
+  title: "whistler",
+  latitude: 50.1163,
+  longitude: -122.9574,
 )
