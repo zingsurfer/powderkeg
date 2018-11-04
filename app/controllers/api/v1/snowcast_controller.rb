9a59b6a7495553978a6c368c501df79f4a1b0af7
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-03 19:45:24 -0600

    Add method to Api::V1::Snowcast index [#1]

--- app/controllers/api/v1/snowcast_controller.rb
+++ app/controllers/api/v1/snowcast_controller.rb
@@ -1,4 +1,14 @@
 class Api::V1::SnowcastController < ApplicationController
   def index
+    conn = Faraday.new(url: "https://api.darksky.net") do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
+    latitude = 42.3601
+    longitude = -71.0589
+    response = conn.get("/forecast/#{ENV["DARKSKY_API_KEY"]}/#{latitude},#{longitude}")
+    binding.pry
+    
+
+    render json: Snowcast.all
   end
 end
