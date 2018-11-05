279d5ad4c4eeb8299cb11b528880dcf065b25031
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-05 12:20:59 -0700

    Add expectation to user request spec [#13]

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 describe 'POST /api/v1/users' do
   it 'sends the user an api_key' do
-    post "/api/v1/users"
-    
+    user_params = { email: "Leia@email.com", password: "Han Solo", password_confirmation: "Han Solo" }
+    post "/api/v1/users", params: { user: user_params }
+    user = User.last
+
     expect(response).to be_successful
+    expect(user.email).to eq(user_params[:email])
   end
 end
 # POST /api/v1/users
