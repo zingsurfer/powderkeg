c83c379efffdaaa4d03aaadb12c115dadbaedb27
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-05 14:13:26 -0700

    Add method to create user [#13]

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    render json: User.create(user_params)
+  end
 
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
