69384da66d2492a5709b970dff633ad30724a2b7
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-04 19:33:02 -0700

    Add validation tests for Resort & make tests pass [#10]

--- app/models/resort.rb
+++ app/models/resort.rb
@@ -1,4 +1,7 @@
 class Resort < ApplicationRecord
   geocoded_by :address
-  after_validation :geocode
+  before_validation :geocode
+  validates_presence_of :name
+  validates_presence_of :latitude
+  validates_presence_of :longitude
 end
