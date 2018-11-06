4bb549c15d26ff7070124edd4bb1fdf1581e07dd
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-05 22:08:59 -0700

    Add method call to authenticate user and send an api key [#13]

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,18 @@
 class Api::V1::UsersController < ApplicationController
-  def create
-    render json: User.create(user_params)
+  skip_before_action :authenticate_request
+
+  def create # sends a token for user if succeeds
+    user = User.create(user_params)
+    command = AuthenticateUser.call(user.email, user.password)
+
+    if command.success?
+      render json: { api_key: command.result }
+    else
+      render json: { error: command.errors }, status: :unauthorized
+    end
   end
 
+  private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
