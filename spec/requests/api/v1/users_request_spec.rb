3f064c7f7dca1c226de7daebd0b6c07930c79e0c
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-05 22:16:21 -0700

    Add expectation for api key [#13]

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 describe 'POST /api/v1/users' do
   it 'sends the user an api_key' do
-    user_params = { email: "Leia@email.com", password: "Han Solo", password_confirmation: "Han Solo" }
+    user_params = { name: "Leia", email: "Leia@email.com", password: "Han Solo", password_confirmation: "Han Solo" }
     post "/api/v1/users", params: { user: user_params }
     user = User.last
 
+    user_api_key = JSON.parse(response.body)
     expect(response).to be_successful
     expect(user.email).to eq(user_params[:email])
+    expect(user_api_key).to have_key("api_key")
   end
 end
 # POST /api/v1/users
