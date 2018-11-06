01e50b8d94e28c4439fe722a9e311104fe012600
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-06 10:20:44 -0700

    Add methods to Api::V1::ImagesController show action [#12]

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -2,5 +2,9 @@ class Api::V1::ImagesController < ApplicationController
   skip_before_action :authenticate_request
 
   def show
+    image_data = Api::V1::UnsplashService.new.random_photo_data
+    image = Api::V1::Image.new(image_data)
+    image_hash = Api::V1::ImageSerializer.new(image).serializable_hash
+    render json: image_hash
   end
 end
