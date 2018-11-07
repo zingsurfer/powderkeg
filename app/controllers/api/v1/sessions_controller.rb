5806f1d3c21269855526b39e6bb92fc6954ae572
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-06 20:06:46 -0700

    Add methods to authenticate user [#14]

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,20 @@
 class Api::V1::SessionsController < ApplicationController
   skip_before_action :authenticate_request
   def create
+    authenticate params[:email], params[:password]
   end
+
+  private
+  def authenticate(email, password)
+    command = AuthenticateUser.call(email, password)
+    if command.success?
+      render json: {
+        api_key: command.result,
+        message: 'Successfully logged in.'
+      }
+    else
+      render json: { error: command.errors }, status: :unauthorized
+    end
+  end
+
 end
