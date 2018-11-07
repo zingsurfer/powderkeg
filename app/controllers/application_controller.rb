7d6bbadae8120c5848446bb5a5079d1a21990408
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-07 11:10:59 -0700

    Remove unnec code [#20]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,8 @@
 class ApplicationController < ActionController::API
-  before_action :authenticate_request, :set_headers
+  before_action :set_headers
   attr_reader :current_user
 
   private
-  def authenticate_request
-    @current_user = AuthorizeApiRequest.call(request.headers).result
-    render json: { error: 'Not Authorized.' }, status: 401 unless @current_user
-  end
 
   def set_headers
     response.set_header("Accept", "application/json")
--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -4,12 +4,5 @@ class JsonWebToken
       payload[:exp] = exp.to_i
       JWT.encode(payload, Rails.application.credentials.secret_key_base)
     end
-
-    def decode(token)
-      body = JWT.decode(token, Rails.application.credentials.secret_key_base, 'none')[0]
-      HashWithIndifferentAccess.new body
-    rescue
-      puts "DOES NOT WORK!!!"
-    end
   end
 end
