87f68af93c5d1ffd9a55918167d1f39658f0bced
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-06 19:21:38 -0700

    Add expectations & method to create user [#14]

--- spec/requests/api/v1/sessions_request_spec.rb
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -4,12 +4,18 @@ describe 'POST /api/v1/sessions' do
   it 'returns the users api_key' do
     user_params = { name: "Leia", email: "Leia@email.com", password: "Han Solo", password_confirmation: "Han Solo" }
     post "/api/v1/users", params: { user: user_params }
+    
     user = User.last
-
-    user_login_params = { email: user.email, password: user.password }
+    user_login_params = { email: user.email, password: "Han Solo" }
     post "/api/v1/sessions", params: user_login_params
 
+    login_response = JSON.parse(response.body)
+
     expect(response).to be_successful
+    expect(response).to have_http_status(200)
+    expect(login_response).to have_key("api_key")
+    expect(login_response).to have_key("message")
+    expect(login_response["message"]).to eq("Successfully logged in.")
   end
 end
 # POST /api/v1/sessions
