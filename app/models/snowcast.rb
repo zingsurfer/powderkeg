624bd7d8f8f62fb5464e13ee4b2d3c1454f10a1e
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-06 19:18:56 -0700

    Resolve conflict [#14]

---
+++ app/models/snowcast.rb
@@ -0,0 +1,20 @@
+class Snowcast
+  def initialize(data = {})
+    @feel_temp = data["currently"]["apparentTemperature"]
+     # @apparent_temp
+     # @apparent_temp_max
+     # @apparent_temp_max_time
+     # @cloud_cover
+     # @precip_accumulation
+     # @precip_intensity_max
+     # @precip_intensity_max_time
+     # @precip_probability
+     # @precip_type
+     # @visibility
+     # @temp_max
+     # @temp_max_time
+     # @wind_gust
+     # @wind_gust_time
+     # @wind_speed
+  end
+end
